f6e20ba71df8f3b07212b0f87e8f696e342a3d63
InKi Park <park28525@gmail.com>
2023-08-17 18:39:42 +0900

    Practice Event

--- Practice/main.js
+++ Practice/main.js
@@ -1,5 +1,18 @@
-const fs = require('fs');
+const EventEmitter = require('events');
 
-fs.readFile('test.js', 'utf8', (err, content)=>{
-  console.log(content);
+const myEmitter = new EventEmitter();
+const myEmitter2 = new EventEmitter();
+
+myEmitter.on('test',() =>{
+  console.log('1');
 });
+
+myEmitter.on('test',() =>{
+  console.log('2');
+});
+
+myEmitter2.on('test',() =>{
+  console.log('3');
+});
+
+myEmitter2.emit('test');
