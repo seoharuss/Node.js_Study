5be1ea9489db69d9dbe4431297b35da07b54b1de
InKi Park <park28525@gmail.com>
2023-08-19 15:41:08 +0900

    study EventEmitter

--- Practice/main.js
+++ Practice/main.js
@@ -1,18 +1,11 @@
 const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
-const myEmitter2 = new EventEmitter();
 
-myEmitter.on('test',() =>{
-  console.log('1');
+const obj = {type : 'text', data: 'hello World', date : '2023-08-19'};
+
+myEmitter.on('test', (arg1) =>{
+  console.log(arg1);
 });
 
-myEmitter.on('test',() =>{
-  console.log('2');
-});
-
-myEmitter2.on('test',() =>{
-  console.log('3');
-});
-
-myEmitter2.emit('test');
+myEmitter.emit('test', obj);
