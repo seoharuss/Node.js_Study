a61a33d8b3cdfc2cc219683e73710f55d9047dec
InKi Park <park28525@gmail.com>
2023-09-04 18:34:04 +0900

    routing practice

--- Server_Practice/main.js
+++ Server_Practice/main.js
@@ -1,6 +1,18 @@
-const http = require('http');
+const http = require("http");
+
+users = ['Tom', 'Andy', 'Jessica', 'Paul'];
 
 let server = http.createServer(function(request, response){
-  response.end('<h1>Hello World!</h1>');
+
+  if(request.url === '/'){
+    response.end('<h1>Welcome!</h1>');
+  }
+  else if(request.url === '/users'){
+    response.end('<h1>' + users + '</h1>');
+  }
+  else{
+    response.end('<h1>Page Not Available</h1>');
+  }
 });
+
 server.listen(3000);
