0e7b0e2f23c3652061b568ddf1e13383c2a838ba
InKi Park <park28525@gmail.com>
2023-09-04 18:42:53 +0900

    routing practice update

--- Server_Practice/main.js
+++ Server_Practice/main.js
@@ -10,6 +10,11 @@ let server = http.createServer(function(request, response){
   else if(request.url === '/users'){
     response.end('<h1>' + users + '</h1>');
   }
+  else if(request.url.split('/')[1] ==='users'){
+    let userIdx = request.url.split('/')[2];
+    let userName = users[userIdx-1];
+    response.end('<h1>' + userName + '</h1>');
+  }
   else{
     response.end('<h1>Page Not Available</h1>');
   }
