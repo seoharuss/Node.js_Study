9b6f9decca5baa6e061bd0c5df6903b701670cc2
InKi Park <park28525@gmail.com>
2023-09-04 19:17:58 +0900

    update style guide

--- Server_Practice/main.js
+++ Server_Practice/main.js
@@ -1,23 +1,22 @@
-const http = require("http");
+const http = require('http'); // etc
 
-users = ['Tom', 'Andy', 'Jessica', 'Paul'];
+const users = ['Tom', 'Andy', 'Jessica', 'Paul']; // const
 
-let server = http.createServer(function(request, response){
-
-  if(request.url === '/'){
+const server = http.createServer((request, response) => { // Arrow Function, const
+  if (request.url === '/') {
     response.end('<h1>Welcome!</h1>');
-  }
-  else if(request.url === '/users'){
-    response.end('<h1>' + users + '</h1>');
-  }
-  else if(request.url.split('/')[1] ==='users'){
-    let userIdx = request.url.split('/')[2];
-    let userName = users[userIdx-1];
-    response.end('<h1>' + userName + '</h1>');
-  }
-  else{
+  } else if (request.url === '/users') {
+    response.end(`<h1>${users}/h1>`); // Template String
+  } else if (request.url.split('/')[1] === 'users') {
+    // url : /users/1, /users/2, .. // etc
+    const userIdx = request.url.split('/')[2];
+    const userName = users[userIdx - 1]; // etc
+
+    response.end(`<h1>${userName}</h1>`); // Template String
+  } else {
     response.end('<h1>Page Not Available</h1>');
   }
 });
 
 server.listen(3000);
+
